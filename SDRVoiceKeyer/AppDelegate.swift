a42eba79b73033ae4b81bea9842a3c10dde13de0
Peter Bourget <pbourget@gmail.com>
2017-02-10 10:26:17 -0800

    Initial Commit

---
+++ SDRVoiceKeyer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SDRVoiceKeyer
+//
+//  Created by Peter Bourget on 2/10/17.
+//  Copyright © 2017 Peter Bourget. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
