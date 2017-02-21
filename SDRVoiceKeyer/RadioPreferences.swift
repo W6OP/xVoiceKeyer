c4f1485ecb94cc3c453b882fbdeeec1337fee5c0
Peter Bourget <pbourget@gmail.com>
2017-02-21 15:49:28 -0800

    Adding preferences pane for file locations.

---
+++ SDRVoiceKeyer/RadioPreferences.swift
@@ -0,0 +1,18 @@
+//
+//  RadioPreferences.swift
+//  SDRVoiceKeyer
+//
+//  Created by Peter Bourget on 2/21/17.
+//  Copyright © 2017 Peter Bourget. All rights reserved.
+//
+
+import Cocoa
+
+class RadioPreferences: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+} // end class
